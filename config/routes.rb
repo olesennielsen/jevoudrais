17e1891328df8fbd4ac6e2a666b9ddb974b25794
Emil Folino Nielsen <emilfolino@gmail.com>
2013-01-02 13:48:24 +0100

    Changed routing and sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Jevoudrais::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
-  end
-  root :to => "home#index"
+  end  
   devise_for :users
+  
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  
   resources :users
 end
