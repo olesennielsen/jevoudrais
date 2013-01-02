afb8b3bbebeb35f16a0746265b3d390c89624785
Emil Folino Nielsen <emilfolino@gmail.com>
2013-01-02 12:00:45 +0100

    Initial commit with code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jevoudrais::Application.config.secret_token = '845ee0c5409334a0befa4dc29267e00039f959f7e414a68b47f4f4e8cbf92c644691c2a327aa02b3e61a525f054048bcdb0aa00923b2983c4d1d5c27c8b74fdb'
