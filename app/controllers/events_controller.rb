f7de59513f9ceeea337953ede2eb3acae6359ca9
Emil Folino Nielsen <emilfolino@gmail.com>
2013-01-12 11:03:23 +0100

    Added event model and events controller to handle things like weddings and things like that

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,10 @@
+class EventsController < ApplicationController
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112093636) do
+ActiveRecord::Schema.define(:version => 20130112095847) do
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.date     "event_date"
+    t.boolean  "recurring"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "gifts", :force => true do |t|
     t.string   "name"
