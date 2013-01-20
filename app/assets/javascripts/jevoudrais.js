c3d8468a0c496f900881838b59eb3832c3718096
Emil Folino Nielsen <emilfolino@gmail.com>
2013-01-20 15:16:54 +0100

    Fixed problem with no reload of javascript leading to problem with sticky footer

--- app/assets/javascripts/jevoudrais.js
+++ app/assets/javascripts/jevoudrais.js
@@ -1,40 +1,29 @@
 $(document).ready(function() {
-	$(window).bind("load", function() { 
-       
-	       var footerHeight = 0,
-	           footerTop = 0,
-	           $footer = $("#footer"),
-						 container = $("#container");
-           
-	       positionFooter();
-       
-	       function positionFooter() {
-       
-          footerHeight = $footer.height();
-					footerWidth = $footer.width();
-					containerWidth = container.width();
-					
-					leftMargin = ($(window).width() - containerWidth)/2;								
-					
-             footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
-					footerLeft = leftMargin + containerWidth - footerWidth;
-            	if ( ($(document.body).height()+footerHeight) < $(window).height()) {
-                $footer.css({
-                     position: "absolute",
-									top: footerTop,
-									left:footerLeft													
-                })
-            	} else {
-                $footer.css({
-                     position: "static"
-                })
-            	}
-               
-	       }
+	var footerHeight = 0,
+	footerTop = 0,
+	footer = $("#footer"),
+	container = $("#container");
 
-	       $(window)
-	               .scroll(positionFooter)
-	               .resize(positionFooter)
-               
-	});
+	positionFooter();
+
+	function positionFooter() {
+		footerHeight = footer.height();
+		footerWidth = footer.width();
+		containerWidth = container.width();
+
+		leftMargin = ($(window).width() - containerWidth)/2;								
+
+		footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+		footerLeft = leftMargin + containerWidth - footerWidth + "px";
+		
+		footer.css({
+			position: "absolute",
+			top: footerTop,
+			left:footerLeft													
+		})
+	}
+
+	$(window)
+	.scroll(positionFooter)
+	.resize(positionFooter)
 });
