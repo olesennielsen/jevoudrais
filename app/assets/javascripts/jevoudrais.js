5bccae27affb0f9577e138b674d4fd76a9875152
Emil Folino Nielsen <emilfolino@gmail.com>
2013-01-20 14:33:02 +0100

    Small changes to the views and created a sticky footer with jquery

--- app/assets/javascripts/jevoudrais.js
+++ app/assets/javascripts/jevoudrais.js
@@ -1,17 +1,40 @@
-$(document).ready(function() {	
-$('#sign-in-form').hide(); //Initially form wil be hidden.
- $('#show-sign-in-form').click(function() {
-  	$('#show-sign-in-form').hide();//Hides the button
-		$('#show-sign-up-form').hide();
-		$('#sign-in-form').show(500);//Form shows on button click	
-  });
-});
+$(document).ready(function() {
+	$(window).bind("load", function() { 
+       
+	       var footerHeight = 0,
+	           footerTop = 0,
+	           $footer = $("#footer"),
+						 container = $("#container");
+           
+	       positionFooter();
+       
+	       function positionFooter() {
+       
+	                footerHeight = $footer.height();
+									footerWidth = $footer.width();
+									containerWidth = container.width();
+									
+									leftMargin = ($(window).width() - containerWidth)/2;								
+									
+	                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+									footerLeft = leftMargin + containerWidth - footerWidth;
+	               	if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+	                   $footer.css({
+	                        position: "absolute",
+													top: footerTop,
+													left:footerLeft													
+	                   })
+	               	} else {
+	                   $footer.css({
+	                        position: "static"
+	                   })
+	               	}
+               
+	       }
 
-$(document).ready(function() {	
-$('#sign-up-form').hide(); //Initially form wil be hidden.
- $('#show-sign-up-form').click(function() {
-  	$('#show-sign-up-form').hide();//Hides the button
-		$('#show-sign-in-form').hide();
-		$('#sign-up-form').show(500);//Form shows on button click	
-  });
+	       $(window)
+	               .scroll(positionFooter)
+	               .resize(positionFooter)
+               
+	});
 });
